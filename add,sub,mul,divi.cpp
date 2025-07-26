3dd2c569f2b185a4ac4b9f3ca30eebd67a1bf148
NafisaAnjum <nafisaanjum390@gmail.com>
2025-07-26 18:05:36 +0530

    Create add,sub,mul,divi.cpp

---
+++ add,sub,mul,divi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int a=4000;
+int b=6000;
+cout<<"multiplication";
+cout<<a*b<<"\n";
+cout<<"addition";
+cout<<a+b<<"\n";
+cout<<"substration";
+cout<<a-b<<"\n";
+cout<<"divison";
+cout<<a%b<<"\n";
+return 0;
+}
